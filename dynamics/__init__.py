247067806db2b74c3f9765368262e02be0921e2c
Nathan Budd <nibudd@gmail.com>
2016-12-28 15:35:35 -0500

    looks like minor aesthetic changes were made

--- dynamics/__init__.py
+++ dynamics/__init__.py
@@ -1,5 +1,6 @@
 from .lyapunov_element_steering import LyapunovElementSteering
 from .proportional_element_control import ProportionalElementControl
+from .proportional_5element_control import Proportional5ElementControl
 from .thrust_constant import ThrustConstant
 from .two_body import TwoBody
 from .zonal_gravity import ZonalGravity
--- dynamics/utilities/diff_elements_theta_into_p.py
+++ dynamics/utilities/diff_elements_theta_into_p.py
@@ -24,21 +24,19 @@ def diff_elements_theta_into_p(mu, k, X, X_r, angle_idx=[5]):
     ndarray
         Results from diff_elements with the updated p error
     """
+    # append w+f
     X_theta = np.concatenate((X, X[0:, 4:5]+X[0:, 5:6]), 1)
     X_r_theta = np.concatenate((X_r, X_r[0:, 4:5]+X_r[0:, 5:6]), 1)
     angle_idx_aug = angle_idx + [6]
 
     X_d_aug = diff_elements(X_theta, X_r_theta, angle_idx_aug)
-    w_d = X_d_aug[0:, 4:5]
-    f_d = X_d_aug[0:, 5:6]
-    theta_d = np.arccos(np.cos(w_d + f_d))
+    theta_d = X_d_aug[0:, 6:7]
 
     p_r = X_r[0:, 0:1]
     e_r = X_r[0:, 1:2]
     a_r = p_r / (1. - e_r**2)
 
-    # NEED SOME KIND OF CHECK FOR NEGATIVE NUMBERS IN ROOT
-    a_r_new_cubed = (mu / ((mu/a_r**3)**.5 - k*theta_d))
+    a_r_new_cubed = (mu / ((mu/a_r**3)**.5 - k*theta_d)**2)
     a_r_new = np.sign(a_r_new_cubed) * np.fabs(a_r_new_cubed)**(1./3.)
     p_r_new = a_r_new * (1. - e_r**2)
 
